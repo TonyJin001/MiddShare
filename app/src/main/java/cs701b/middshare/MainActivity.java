3ae38cb6ba1735274d89771bbeac7f1c8839ec32
Birgitta Cheng <birgittacheng@birgittas-mbp.middlebury.edu>
2016-10-12 22:55:27 -0400

    created home screen and starting the function screens

--- app/src/main/java/cs701b/middshare/MainActivity.java
+++ app/src/main/java/cs701b/middshare/MainActivity.java
@@ -2,12 +2,25 @@ package cs701b.middshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user clicks Log In Button */
+    public void logIn(View view){
+        //does something in response to button
+        Intent intent = new Intent(this,ServiceExchange.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
