4d04ee5b4c0a26fbd71c430840f1d9cab615cde4
Yanfeng Jin (Tony) <yjin@middlebury.edu>
2016-10-31 00:08:29 -0400

    Fixed the crash problem. Detail view shows comment options

--- app/src/main/java/cs701b/middshare/ServiceExchangeDetails.java
+++ app/src/main/java/cs701b/middshare/ServiceExchangeDetails.java
@@ -3,6 +3,7 @@ package cs701b.middshare;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -20,10 +21,11 @@ public class ServiceExchangeDetails extends AppCompatActivity {
     private Button submitComment;
     private LinearLayout outsideLL;
     private LinearLayout itemInfoLL;
-    private LinearLayout buttomLL;
+    private LinearLayout bottomLL;
+    private final String TAG = "Service Exchange Detail";
 
-    public ServiceExchangeDetails() {
-    }
+//    public ServiceExchangeDetails() {
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +35,35 @@ public class ServiceExchangeDetails extends AppCompatActivity {
     }
 
     private void initView() {
+        Log.d(TAG,"Before outsideLL");
         outsideLL = (LinearLayout) findViewById(R.id.outside_ll);
+        Log.d(TAG,"after outsideLL");
         itemInfoLL = (LinearLayout) outsideLL.findViewById(R.id.item_info_ll);
+        Log.d(TAG,"after itemInfoLL");
         userPhoto = (ImageView) itemInfoLL.findViewById(R.id.user_photo_details);
+        Log.d(TAG,"after userphoto");
         description = (TextView) itemInfoLL.findViewById(R.id.description_details);
+        Log.d(TAG,"after description");
         price = (TextView) itemInfoLL.findViewById(R.id.cost_details);
+        Log.d(TAG,"after price");
         comments = (ListView) outsideLL.findViewById(R.id.comments);
-        buttomLL = (LinearLayout) findViewById(R.id.buttom_ll);
+        Log.d(TAG,"after comments");
+        bottomLL = (LinearLayout) findViewById(R.id.bottom_ll);
+        Log.d(TAG,"after buttomLL");
         writeComment = (EditText) findViewById(R.id.write_comments);
+        Log.d(TAG,"after wrteComment");
         submitComment = (Button) findViewById(R.id.submit_comments);
+        Log.d(TAG,"after submitcomment");
 
         Intent intent = getIntent();
+        Log.d(TAG,"after intent");
         Bundle extras = intent.getExtras();
+        Log.d(TAG,"after extra");
         description.setText(extras.getString("EXTRA_DESCRIPTION"));
+        Log.d(TAG,"after setdescription");
         price.setText(extras.getString("EXTRA_PRICE"));
+        Log.d(TAG,"after setprice");
         new GetProfilePhoto(userPhoto).execute(extras.getString("EXTRA_PHOTOURL"));
+        Log.d(TAG,"after getprofilephoto");
     }
 }
