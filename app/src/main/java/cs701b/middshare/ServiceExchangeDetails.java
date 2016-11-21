85e0c573ab0f385a9a538d31a8e3034e830c00fc
Yanfeng Jin (Tony) <yjin@middlebury.edu>
2016-11-20 21:38:56 -0500

    Add functions to handle notifications, cannot send one yet

--- app/src/main/java/cs701b/middshare/ServiceExchangeDetails.java
+++ app/src/main/java/cs701b/middshare/ServiceExchangeDetails.java
@@ -38,39 +38,24 @@ public class ServiceExchangeDetails extends AppCompatActivity {
     }
 
     private void initView() {
-        Log.d(TAG,"Before outsideLL");
         outsideLL = (LinearLayout) findViewById(R.id.outside_ll);
-        Log.d(TAG,"after outsideLL");
         itemInfoRL = (RelativeLayout) outsideLL.findViewById(R.id.item_info_rl);
-        Log.d(TAG,"after itemInfoLL");
         userPhoto = (ImageView) itemInfoRL.findViewById(R.id.user_photo_details);
-        Log.d(TAG,"after userphoto");
         description = (TextView) itemInfoRL.findViewById(R.id.description_details);
-        Log.d(TAG,"after description");
         price = (TextView) itemInfoRL.findViewById(R.id.cost_details);
         name = (TextView) itemInfoRL.findViewById(R.id.name_details);
         details = (TextView) itemInfoRL.findViewById(R.id.details_details);
-        Log.d(TAG,"after price");
         comments = (ListView) outsideLL.findViewById(R.id.comments);
-        Log.d(TAG,"after comments");
         bottomLL = (LinearLayout) findViewById(R.id.bottom_ll);
-        Log.d(TAG,"after buttomLL");
         writeComment = (EditText) findViewById(R.id.write_comments);
-        Log.d(TAG,"after wrteComment");
         submitComment = (Button) findViewById(R.id.submit_comments);
-        Log.d(TAG,"after submitcomment");
 
         Intent intent = getIntent();
-        Log.d(TAG,"after intent");
         Bundle extras = intent.getExtras();
-        Log.d(TAG,"after extra");
         description.setText(extras.getString("EXTRA_DESCRIPTION"));
-        Log.d(TAG,"after setdescription");
         price.setText(extras.getString("EXTRA_PRICE"));
         name.setText(extras.getString("EXTRA_NAME"));
         details.setText(extras.getString("EXTRA_DETAILS"));
-        Log.d(TAG,"after setprice");
         new GetProfilePhoto(userPhoto).execute(extras.getString("EXTRA_PHOTOURL"));
-        Log.d(TAG,"after getprofilephoto");
     }
 }
