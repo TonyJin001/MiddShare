5894eb69eed2c8df451232a91b10bc0e3fa76f69
Cheng <ycheng@equinox.cs.middlebury.edu>
2016-12-07 23:49:19 -0500

    geofencing: location grab

--- app/src/main/java/cs701b/middshare/ServiceExchange.java
+++ app/src/main/java/cs701b/middshare/ServiceExchange.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
@@ -31,6 +32,14 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -69,10 +78,12 @@ public class ServiceExchange extends AppCompatActivity {
     private final String TAG = "Service_Exchange";
     private Bitmap currentBitmap = null;
     private LruCache<String, Bitmap> mMemoryCache;
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
+    private GoogleApiClient mGoogleApiClient;
     private GoogleApiClient client;
 
     @Override
@@ -80,7 +91,6 @@ public class ServiceExchange extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_exchange);
         // hasn't verified isRegistered... yet
-
 
         final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
         final int cacheSize = maxMemory / 8;
@@ -169,8 +179,8 @@ public class ServiceExchange extends AppCompatActivity {
                 @Override
                 protected void populateView(View v, ServiceExchangeItemNoTime model, int position) {
                     DatabaseReference ref = this.getRef(position);
-                    if (model.getTimeLimit() < Calendar.getInstance().getTimeInMillis() && model.getTimeLimit()!=-1){
-                        Log.v(TAG,"Time limit passed");
+                    if (model.getTimeLimit() < Calendar.getInstance().getTimeInMillis() && model.getTimeLimit() != -1) {
+                        Log.v(TAG, "Time limit passed");
                         ref.removeValue();
                     }
                     ImageView userPhoto = (ImageView) v.findViewById(R.id.user_photo);
@@ -180,7 +190,7 @@ public class ServiceExchange extends AppCompatActivity {
                     TextView buySell = (TextView) v.findViewById(R.id.buy_sell);
                     description.setText(model.getDescription());
                     userName.setText(model.getName());
-                    if (model.isBuy()){
+                    if (model.isBuy()) {
                         buySell.setText("Buying for");
                         buySell.setTextColor(Color.parseColor("#E91E63"));
                     } else {
@@ -217,21 +227,21 @@ public class ServiceExchange extends AppCompatActivity {
                     String itemDetailedInfo = itemDetails.getDetails();
                     String itemKey = adapter.getRef(position).getKey();
                     String itemBuySell = "";
-                    if (itemDetails.isBuy()){
+                    if (itemDetails.isBuy()) {
                         itemBuySell = "Buying for";
                     } else {
                         itemBuySell = "Selling for";
                     }
-                    Log.d(TAG,itemDescription +"\t" + itemPrice + "\t" + itemPhotoUrl);
-                    Intent intent = new Intent(ServiceExchange.this,ServiceExchangeDetails.class);
+                    Log.d(TAG, itemDescription + "\t" + itemPrice + "\t" + itemPhotoUrl);
+                    Intent intent = new Intent(ServiceExchange.this, ServiceExchangeDetails.class);
                     Bundle extras = new Bundle();
                     extras.putString("EXTRA_DESCRIPTION", itemDescription);
                     extras.putString("EXTRA_PRICE", itemPrice);
                     extras.putString("EXTRA_PHOTOURL", itemPhotoUrl);
                     extras.putString("EXTRA_NAME", itemName);
-                    extras.putString("EXTRA_DETAILS",itemDetailedInfo);
-                    extras.putString("EXTRA_ITEM_KEY",itemKey);
-                    extras.putString("EXTRA_BUY_SELL",itemBuySell);
+                    extras.putString("EXTRA_DETAILS", itemDetailedInfo);
+                    extras.putString("EXTRA_ITEM_KEY", itemKey);
+                    extras.putString("EXTRA_BUY_SELL", itemBuySell);
 
                     Log.d(TAG, itemKey);
 
@@ -254,15 +264,42 @@ public class ServiceExchange extends AppCompatActivity {
 //            });
 
 
-
-
             startService(new Intent(this, NotificationListener.class));
 
         }
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnected(Bundle bundle) {
+                            Log.d(TAG, "Connected to GoogleApiClient");
+                        }
+
+                        @Override
+                        public void onConnectionSuspended(int i) {
+                            Log.d(TAG, "Suspended connection to GoogleApiClient");
+                        }
+                    })
+                   /* .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener(){
+                        @Override
+                        public void onConnectionFailed(ConnectionResult result) {
+                            Log.d(TAG,"Failed to connect to GoogleApiClient-" + result.getErrorMessage());
+                        }
+                    })
+                    */
+                    .build();
+        }
+
+      //  startLocationMonitoring();
     }
+
 
     private void updateProfile(String name, String email, Uri uri) {
         UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
@@ -310,6 +347,25 @@ public class ServiceExchange extends AppCompatActivity {
         }
     }
 
+    private void startLocationMonitoring() {
+        Log.d(TAG, "startLocation called");
+        try {
+            LocationRequest locationRequest = LocationRequest.create()
+                    .setInterval(10000)
+                    .setFastestInterval(5000)
+                    //.setNumUpdates(5)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, new LocationListener(){
+                public void onLocationChanged(Location location){
+                    Log.d(TAG, "Location update lat/long" + location.getLatitude() + " " + location.getLongitude());
+                }
+            });
+        } catch (SecurityException e) {
+             Log.d(TAG, "SecurityException - " + e.getMessage());
+        }
+    }
+
     public Bitmap getBitmapFromMemCache(String key) {
         return mMemoryCache.get(key);
     }
@@ -331,23 +387,61 @@ public class ServiceExchange extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        Log.d(TAG,"onResume called");
+        super.onResume();
+
+        int response = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (response != ConnectionResult.SUCCESS) {
+            Log.d(TAG,"Google Play Services not available - show dialog to ask user to download it");
+            GoogleApiAvailability.getInstance().getErrorDialog(this,response,1).show();
+
+        } else {
+            Log.d(TAG,"Google Play Services is available- no action is required");
+        }
+    }
+    @Override
     public void onStart() {
         super.onStart();
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mGoogleApiClient.reconnect();
         client.connect();
         AppIndex.AppIndexApi.start(client, getIndexApiAction());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+       // AppIndex.AppIndexApi.start(mGoogleApiClient, getIndexApiAction0());
     }
 
     @Override
     public void onStop() {
-        super.onStop();
-
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+       // AppIndex.AppIndexApi.end(mGoogleApiClient, getIndexApiAction0());
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mGoogleApiClient.disconnect();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction0() {
+        Thing object = new Thing.Builder()
+                .setName("ServiceExchange Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
 
 
