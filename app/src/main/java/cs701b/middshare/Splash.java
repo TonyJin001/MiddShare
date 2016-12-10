686cea0d352b3d0e074ee300979e3427d8457fdf
Jin <yjin@ellen.cs.middlebury.edu>
2016-12-10 17:00:53 -0500

    Added splash screen (not doing anything in background thought

---
+++ app/src/main/java/cs701b/middshare/Splash.java
@@ -0,0 +1,26 @@
+package cs701b.middshare;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private final int SPLASH_LEN = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        },SPLASH_LEN);
+    }
+}
